13e860725f48129631205453084b5a3d035e4fec
Ben Hastings <ben.hastings@gmail.com>
2020-10-12 14:33:01 -0400

    parameters added

--- seriesGenerator.js
+++ seriesGenerator.js
@@ -7,19 +7,29 @@ const obsLength = +process.argv[3];
 
 const startDT = Math.floor(Date.now() / 5000) * 5000 - 86400000;
 
-const returnString = (le, value, timestamp, entries = false) => {
+const returnString = (le, valueArray, timeStampArray) => {
   console.log(entries);
-  return `{"metric":{"__name__":"latency_secondsValue","env":"perf","app":"testing","le":"${le}"},"values":[${value}],"timestamps":[${timestamp}]}`;
+  return `{"metric":{"__name__":"latency_secondsValue","env":"perf","app":"testing","le":"${le}"},"values":[${valueArray}],"timestamps":[${timeStampArray}]}`;
 };
 
 const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-let value = getRandomInt(30, 300);
+const fakeArrayEntries = (input) => {
+  const signValue = Math.round(Math.random());
+  const nextIncrement = getRandomInt(input * 0.05, input * 0.1);
+  const nextEntry =
+    signValue === 1 ? input + nextIncrement : input - nextIncrement;
+  return nextEntry;
+};
+
+let value = getRandomInt(0, 300);
 let jsonl = "";
 buckets.forEach((element, idx) => {
-  console.log("forEach idx:", idx);
+  // console.log("forEach idx:", idx);
+  const valueArray = [];
+  const timeStampArray = [startDT]
   if (idx === 0) {
     value = value;
   } else if (idx % 5 === 0) {
@@ -27,14 +37,24 @@ buckets.forEach((element, idx) => {
   } else {
     value = value + getRandomInt(5, 10);
   }
-  jsonl += returnString(element, value, startDT) + "\n";
+
+  valueArray.push(value)
+  if obsLength > 0 {
+    while (valueArray.length <= obsLength) {
+      valueArray.push(fakeArrayEntries(value))
+      timeStampArray.push(timeStampArray[timeStampArray.length-1]+5000);
+    }
+  }
+
+  jsonl += returnString(element, valueArray, timeStampArray) + "\n";
 });
 // form.append()
-console.log(jsonl);
+console.log("\nlength of metrics to send: ", jsonl.length,"\n",jsonl.substring(jsonl.length-300,jsonl.length));
 
-got.post("http://localhost:8428/api/v1/import", {
+(async()=>{ const {body} = await got.post(`http://${vmHost}/api/v1/import`, {
   body: jsonl,
 });
-
+console.log('responseBody:\n',body)
+})();
 //  Test response
 // curl -G 'http://localhost:8428/api/v1/export' -d 'match={__name__=~"latency_.*"}'
